f05a5a1d1e41c5af773ac7f1019f41e48e0ce6d0
Zodiax75 <martin.nassler@gmail.com>
2021-07-02 23:26:16 +0200

    Add project files.

---
+++ HowManyTimes/HowManyTimes/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HowManyTimes.Views;
+
+namespace HowManyTimes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            
+            Sharpnado.Shades.Initializer.Initialize(loggerEnable: false);
+            Device.SetFlags(new string[] { "Shapes_Experimental" });
+            
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HowManyTimes/HowManyTimes/Views/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace HowManyTimes.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnNewCounterClicked(object sender, EventArgs e)
+        {
+            MainThread.InvokeOnMainThreadAsync(async () => await AnimateMe());
+        }
+
+        private async Task AnimateMe()
+        {
+            new Animation
+        {
+            { 0.5, 0.8, new Animation(v => hashImage.FadeTo(0)) },
+            { 0.99, 1, new Animation(v => hashImage.IsVisible = false) }
+        }.Commit(this, "leftArrowTransitionAnimation", 60, 350, Easing.Linear);
+
+            await Task.Delay(750);
+
+            await Application.Current.MainPage.Navigation.PushAsync(new DetailPage(), false);
+        }
+    }
+}
