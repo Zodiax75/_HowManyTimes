c1d8d85b8b06a9f67b62de572c30b82e07ba1dd4
Zodiax75 <martin.nassler@gmail.com>
2021-09-14 23:12:21 +0200

    added support for awesome fonts

--- HowManyTimes/HowManyTimes/AssemblyInfo.cs
+++ HowManyTimes/HowManyTimes/AssemblyInfo.cs
@@ -2,7 +2,11 @@ using Xamarin.Forms.Xaml;
 using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+// Fonts
 [assembly: ExportFont("Poppins-Regular.ttf", Alias = "FontPoppinsRegular")]
 [assembly: ExportFont("Poppins-Medium.ttf", Alias = "FontPoppinsMedium")]
 [assembly: ExportFont("Poppins-SemiBold.ttf", Alias = "FontPoppinsSemiBold")]
 [assembly: ExportFont("fontello.ttf", Alias = "FontFontello")]
+
+[assembly: ExportFont("Font Awesome 5 Free-Solid-900.otf", Alias = "FontAwesome5Regular")]
---
+++ HowManyTimes/HowManyTimes/Services/Converters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HowManyTimes.Services
+{
+    /// <summary>
+    /// Converter for favorite icon background
+    /// </summary>
+    public class FavoriteIconConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool && value != null)
+            {
+                bool s = (bool)value;
+                switch (s)
+                {
+                    case true:
+                        return "#D4AF37";
+                    default:
+                        return "#6F6F6F";
+                }
+            }
+
+            return "#6F6F6F";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
