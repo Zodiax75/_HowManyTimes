16cd3e480de7c016d74973c8395fefc3a788e5c3
Zodiax75 <martin.nassler@gmail.com>
2021-11-11 06:58:48 +0100

    New design of homepage

--- HowManyTimes/HowManyTimes/Views/MainPage.xaml.cs
+++ HowManyTimes/HowManyTimes/Views/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using HowManyTimes.Models;
 using HowManyTimes.Services;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace HowManyTimes.Views
 {
@@ -14,6 +15,8 @@ namespace HowManyTimes.Views
         {
             InitializeComponent();
             BindingContext = new MainPageViewModel();
+
+            mainHeader.BackgroundColor = Color.Transparent;
         }
 
         /// <summary>
@@ -23,8 +26,39 @@ namespace HowManyTimes.Views
         /// <param name="e"></param>
         public async void OnNewCounterClicked(object sender, EventArgs e)
         {
+            if (newCounter.IsVisible)
+            {
+                newCounter.FadeTo(0, 1500);
+                newCounter.IsVisible = false;
+            }
+            else
+            {
+                newCounter.IsVisible = true;
+                newCounter.FadeTo(1, 1500);
+            }
+        }
+
+        public async void OnSwipped(object sender, EventArgs e)
+        {
             // TODO: rework to use Command and bind it to ViewModel
             await Application.Current.MainPage.Navigation.PushAsync(new DetailPage(), true);
+        }
+
+        public async void OnSearchTapped(object sender, EventArgs e)
+        {
+            // hide if shown and show if hidden
+            searchFrame.IsVisible = !searchFrame.IsVisible;
+
+            // set icon color
+            switch(searchFrame.IsVisible)
+            {
+                case true:
+                    searchIcon.TextColor = Color.FromHex("#FF8920");
+                    break;
+                case false:
+                    searchIcon.TextColor = Color.FromHex("#6F6F6F");
+                    break;
+            }
         }
     }
 }
